370acdce01cb45148ae0019e5905760a17af52c4
Md Shehab Kazi <shehabkazi@gmail.com>
2026-09-14 02:23:33 +0600

    Auto-format terraform files using terraform fmt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "public" {
 # --- Private subnets (EKS worker nodes live here) ---
 resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
-  vpc_id             = aws_vpc.this.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
 
