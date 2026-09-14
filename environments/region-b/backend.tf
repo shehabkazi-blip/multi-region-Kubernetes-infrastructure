415208277194249b1c74cadce1b7f975fd96eb2b
Md Shehab Kazi <shehabkazi@gmail.com>
2026-09-14 13:24:30 +0600

    Add OIDC smoke test workflow, fix region-b dynamodb table name

--- environments/region-b/backend.tf
+++ environments/region-b/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-shehab-2026"
     key            = "multi-region-k8s-infrastructure/region-b/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-lock-table"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
