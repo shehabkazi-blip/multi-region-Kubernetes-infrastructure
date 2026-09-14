9e1f7bdc44a811c4224a406efc3b2e2adbfd1666
Md Shehab Kazi <shehabkazi@gmail.com>
2026-09-14 15:47:36 +0600

    Fix EKS node group: set ami_type to AL2023

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -107,6 +107,7 @@ resource "aws_eks_node_group" "this" {
 
   instance_types = var.node_instance_types
   capacity_type  = var.node_capacity_type
+  ami_type       = "AL2023_x86_64_STANDARD"
 
   scaling_config {
     desired_size = var.node_desired_size
