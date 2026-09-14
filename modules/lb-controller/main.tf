543f49d31d2ef21c1ef2bedb2ff9040ae6b46569
Md Shehab Kazi <shehabkazi@gmail.com>
2026-09-15 00:22:35 +0600

    fix: add kubernetes provider and data sources to lb-controller main module

--- modules/lb-controller/main.tf
+++ modules/lb-controller/main.tf
@@ -1,20 +1,6 @@
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 data "aws_iam_policy_document" "lb_controller_assume_role" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
